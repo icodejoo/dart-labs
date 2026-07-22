93936db496088c9eb47046ae45524d4eb0b749a5
icodejoo <icodejoo@users.noreply.github.com>
2026-07-22 07:26:33 +0000

    deploy: dc633823354c526f75c44c0b9c02267d06c9d499

--- cacheman/flutter_bootstrap.js
+++ cacheman/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2008691065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1904909862" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
