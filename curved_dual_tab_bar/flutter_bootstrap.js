68c0a82447d8509a6b829f15c1024699638c1bfc
icodejoo <icodejoo@users.noreply.github.com>
2026-09-14 08:15:52 +0000

    deploy: d5f719e5f48425ee0f0482424e30535d7c42c303

--- curved_dual_tab_bar/flutter_bootstrap.js
+++ curved_dual_tab_bar/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1440547629" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "271058664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
